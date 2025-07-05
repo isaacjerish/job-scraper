d59f5de8d59fd396b6841f947311e0a801417083
Isaac Jerish <152412476+isaacjerish@users.noreply.github.com>
2025-07-04 19:35:46 -0500

    Redid file setup

--- api/greenhouse.py
+++
@@ -1,7 +0,0 @@
-def scrape_greenhouse():
-    """
-    Scrapes job listings from selected Greenhouse-hosted companies.
-    Returns a list of job dicts with keys: title, link, location, company.
-    """
-    # TODO: Implement with requests + BeautifulSoup
-    return []
